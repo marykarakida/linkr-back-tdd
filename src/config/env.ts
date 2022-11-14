58f07b40c090de76df5a184b81bba759fd0b2a38
Mary Karakida <makarakida@gmail.com>
2022-11-14 14:33:04 -0300

    fix: use .env to load env variables if NODE_ENV not provided

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 
 export function loadEnv() {
-  const currentEnv = dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+  const path = process.env.NODE_ENV ? `./.env.${process.env.NODE_ENV}` : './.env';
+  const currentEnv = dotenv.config({ path });
 
   if (currentEnv.error) {
     throw new Error(currentEnv.error.message);
