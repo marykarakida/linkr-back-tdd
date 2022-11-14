2a0ded1b4b214d3060c865c0535a0001193b358e
Mary Karakida <makarakida@gmail.com>
2022-11-14 15:55:58 -0300

    fix: fix husky

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(json());
 app.use(morgan('dev'));
 
-console.log('sss');
-
 app.get('/health', (_req, res) => res.json({ message: 'App is up and running!' }));
 
 export default app;
