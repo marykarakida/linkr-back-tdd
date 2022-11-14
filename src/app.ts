a792db8f1baaf11d279525778431e154dd3f8cb4
Mary Karakida <makarakida@gmail.com>
2022-11-14 14:58:06 -0300

    add morgan
    - use "dev" pre-defined format"

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+
 import { loadEnv } from './config/env';
 
 loadEnv();
@@ -8,5 +10,6 @@ const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(morgan('dev'));
 
 export default app;
