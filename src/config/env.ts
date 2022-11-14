b56ebcd09bab7f080c9e0193072771b32d7f6aa5
Mary Karakida <makarakida@gmail.com>
2022-11-14 14:20:50 -0300

    chore: throw error if provided env file does not exist

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,5 +3,10 @@ import dotenvExpand from 'dotenv-expand';
 
 export function loadEnv() {
   const currentEnv = dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+
+  if (currentEnv.error) {
+    throw new Error(currentEnv.error.message);
+  }
+
   dotenvExpand.expand(currentEnv);
 }
